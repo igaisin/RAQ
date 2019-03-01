d74ac3cd90cdf5dcde4b55d4bedf466f2b70876d
Ilyas Gaisin <igaisin@users.noreply.github.com>
2019-03-01 20:44:53 +0600

    Fix

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -1,15 +1,9 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
-
-
-// var phrasePara = document.querySelector('.phrase');
 var speechResult = document.getElementById('speechResult');
 var answer = document.getElementById('answer');
 
 var lastResult = '';
 
-var recognition = new SpeechRecognition();
+var recognition = new webkitSpeechRecognition();
 recognition.lang = 'ar';
 recognition.interimResults = true;
 recognition.continuous = true;
@@ -42,15 +36,9 @@ recognition.onerror = function(event) {
   answer.innerHTML = "";
 }
 
-// recognition.onaudiostart = function(event) {
-//     //Fired when the user agent has started to capture audio.
-//     console.log('SpeechRecognition.onaudiostart');
-// }
+// recognition.onaudiostart = function(event) {}
 
-// recognition.onaudioend = function(event) {
-//     //Fired when the user agent has finished capturing audio.
-//     console.log('SpeechRecognition.onaudioend');
-// }
+// recognition.onaudioend = function(event) {}
 
 recognition.onend = function(event) {
   document.getElementById('btnMicStop').style.display = 'none';
@@ -58,15 +46,9 @@ recognition.onend = function(event) {
   speechResult.innerHTML = "Читай ещё!";
 }
 
-// recognition.onnomatch = function(event) {
-//     //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-//     console.log('SpeechRecognition.onnomatch');
-// }
+// recognition.onnomatch = function(event) {}
 
-// recognition.onspeechstart = function (event) {
-//     //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-//     console.log('SpeechRecognition.onspeechstart');
-// }
+// recognition.onspeechstart = function (event) {}
 
 recognition.onstart = function(event) {
   speechResult.innerHTML = "Слушаю!";
