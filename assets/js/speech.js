1ec4a6674e32f5026cccfeff4e8e86b3e46583eb
Ilyas Gaisin <igaisin@users.noreply.github.com>
2019-03-02 08:50:10 +0600

    Update speech.js

--- assets/js/speech.js
+++ assets/js/speech.js
@@ -1,67 +1,183 @@
-var speechResult = document.getElementById('speechResult');
-var answer = document.getElementById('answer');
+var Speech = (function (undefined) {
 
-var lastResult = '';
+  var eventSplitter = /\s+/,
+      Events = function () {}
 
-var recognition = new webkitSpeechRecognition();
-recognition.lang = 'ar';
-recognition.interimResults = true;
-recognition.continuous = true;
+  Events.prototype.on = function (events, callback, context) {
+      var cache, event, list
+      if (!callback) return this
 
-recognition.onresult = function(event) {
-  var final = "";
-  var interim = "";
-  for (var i = 0; i < event.results.length; ++i) {
-    if (event.results[i].isFinal) {
-      final += event.results[i][0].transcript;
-    } else {
-      interim += event.results[i][0].transcript;
-    }
+      cache = this.__events || (this.__events = {})
+      events = events.split(eventSplitter)
+
+      while (event = events.shift()) {
+          list = cache[event] || (cache[event] = [])
+          list.push(callback, context)
+      }
+
+      return this
   }
-  if (final != "") {
-    lastResult = final;
-  } else {
-    speechResult.innerHTML = interim;
+
+  Events.prototype.off = function (events, callback, context) {
+      var cache, event, list, i
+
+      if (!(cache = this.__events)) return this
+      if (!(events || callback || context)) {
+          delete this.__events
+          return this
+      }
+
+      events = events ? events.split(eventSplitter) : Object.keys(cache)
+
+      while (event = events.shift()) {
+          list = cache[event]
+          if (!list) continue
+
+          if (!(callback || context)) {
+              delete cache[event]
+              continue
+          }
+
+          for (i = list.length - 2; i >= 0; i -= 2) {
+              if (!(callback && list[i] !== callback ||
+                      context && list[i + 1] !== context)) {
+                  list.splice(i, 2)
+              }
+          }
+      }
+
+      return this
   }
-}
 
-recognition.onspeechend = function() {
-  
-}
+  Events.prototype.emit = function(events) {
+      var cache, event, all, list, i, len, rest = [], args
+      if (!(cache = this.__events)) return this
 
-recognition.onerror = function(event) {
-  document.getElementById('btnMicStop').style.display = 'none';
-  document.getElementById('btnMicStart').style.display = 'block';
-  speechResult.innerHTML = 'Прочитай еще раз! Ошибка:' + event.error;
-  answer.innerHTML = "";
-}
+      events = events.split(eventSplitter)
 
-// recognition.onaudiostart = function(event) {}
+      for (i = 1, len = arguments.length; i < len; i++) {
+          rest[i - 1] = arguments[i]
+      }
 
-// recognition.onaudioend = function(event) {}
+      while (event = events.shift()) {
+          if (all = cache.all) all = all.slice()
+          if (list = cache[event]) list = list.slice()
 
-recognition.onend = function(event) {
-  document.getElementById('btnMicStop').style.display = 'none';
-  document.getElementById('btnMicStart').style.display = 'block';
-  speechResult.innerHTML = "Читай ещё!";
-}
+          if (list) {
+              for (i = 0, len = list.length; i < len; i += 2) {
+                  list[i].apply(list[i + 1] || this, rest)
+              }
+          }
 
-// recognition.onnomatch = function(event) {}
+          if (all) {
+              args = [event].concat(rest)
+              for (i = 0, len = all.length; i < len; i += 2) {
+                  all[i].apply(all[i + 1] || this, args)
+              }
+          }
+      }
 
-// recognition.onspeechstart = function (event) {}
+      return this
+  }
 
-recognition.onstart = function(event) {
-  speechResult.innerHTML = "Слушаю!";
-  answer.innerHTML = "Ищу ответ!";
-  document.getElementById('btnMicStart').style.display = 'none';
-  document.getElementById('btnMicStop').style.display = 'block';
-}
+  Events.mixTo = function(receiver) {
+      receiver = receiver.prototype || receiver
+      var proto = Events.prototype
 
+      for (var p in proto) {
+          if (proto.hasOwnProperty(p)) {
+              receiver[p] = proto[p]
+          }
+      }
+  }
 
-function recStart() {  
-  recognition.start();
-}
+  function Speech (options) {
 
-function recStop() { 
-  recognition.stop();
-}
+      this.options = {
+          debugging: false,
+          continuous: false,
+          interimResults: false,
+          autoRestart: false
+      }
+
+      if (Object.prototype.toString.call(options) === '[object Object]') {
+          for (var op in options) {
+              this.options[op] = options[op]
+          }
+      }
+
+      this.active         = false
+      this.manualStopped  = false
+      this.history        = []
+      this.lastIndex      = -1
+      this.lastResult     = ''
+      this.recognition    = new webkitSpeechRecognition()
+
+      var rec = this.recognition,
+          self = this
+
+      rec.continuous = true
+      rec.interimResults = true      
+      rec.lang = 'ar'
+
+      rec.onstart = function () {
+          self.active = true
+          this.manualStopped = false
+          self.emit('start')
+      }
+
+      rec.onresult = function (e) {
+          if (!e.results || !e.results.length) return
+
+          var updatedResult = e.results[e.resultIndex],
+              transcript = updatedResult[0].transcript.replace(/^\s*/, '')
+
+          
+          if (e.resultIndex !== self.lastIndex) {
+              self.lastIndex = e.resultIndex
+              self.lastResult = ''
+          }
+
+          if (transcript === self.lastResult && !updatedResult.isFinal) return
+          if (transcript.length < self.lastResult.length) return
+
+          self.lastResult = transcript
+
+          if (updatedResult.isFinal) {
+              self.history.push(transcript)
+              self.emit('finalResult', transcript)
+          } else {
+              self.emit('interimResult', transcript)
+          }
+      }
+
+      rec.onerror = function (e) {
+          self.emit('error', e)
+      }
+
+      rec.onend = function () {
+          self.active = false
+          self.lastIndex  = -1
+      }
+  }
+
+  Speech.prototype.start = function () {
+      if (this.active) return
+      document.getElementById("btnMicStart").style.display = 'none'
+      document.getElementById("btnMicStop").style.display = 'block'
+      this.recognition.start()
+  }
+
+  Speech.prototype.stop = function () {
+      if (!this.active) return
+      this.manualStopped = true
+      document.getElementById("btnMicStart").style.display = 'block'
+      document.getElementById("btnMicStop").style.display = 'none'
+      this.recognition.stop()
+  }
+
+  Events.mixTo(Speech)
+
+  return Speech
+
+})()
